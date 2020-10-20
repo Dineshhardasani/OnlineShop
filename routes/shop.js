cef3ed3ead76e67e512681104e7c3de0eaf5648f
Dinesh Hardasani <dineshhardasani2000@gmail.com>
2020-10-20 19:41:08 +0530

    Checkout route

--- routes/shop.js
+++ routes/shop.js
@@ -18,6 +18,12 @@ router.get('/cart', isAuth,shopController.getCart);
 
 router.post('/cart',isAuth,shopController.postCart);
 
+router.get('/checkout',isAuth,shopController.getCheckout);
+
+router.get('/checkout/success',shopController.postOrder);
+
+router.get('/checkout/cancel',shopController.getCheckout);
+
 router.post('/cart-delete-item',isAuth,shopController.postCartDeleteProduct);
 
 router.get('/orders', isAuth,shopController.getOrders);
